5fa9d471c5c53f8ce32c12783c05aa427f77445d
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-07 12:58:26 +0600

    add cash in screen add

---
+++ lib/screens/add_cash_in.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddCashIn extends StatefulWidget {
+  const AddCashIn({super.key});
+
+  @override
+  State<AddCashIn> createState() => _AddCashInState();
+}
+
+class _AddCashInState extends State<AddCashIn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
