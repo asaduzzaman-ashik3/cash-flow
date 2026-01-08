5ba7047d403015925bb697fcc7bc5639177ce369
Assaduzzaman Ashik <ashikhossain372@gmail.com>
2026-01-08 16:32:50 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -463,7 +463,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.calculate_outlined),
-            label: 'Net Earning List',
+            label: 'Total List',
           ),
         ],
       ),
